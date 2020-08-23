102f1069c8acc87bfdbc311b8513a6b907ed5d9f
Dora F <doradora.prog@gmail.com>
2020-08-24 04:31:30 +0900

    Add choosing dices when you re-roll dice

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 var url = "ws://" + window.location.host + "/sampleapp/ws";
 var ws = new WebSocket(url);
 var name = "User-" + Math.floor(Math.random() * 1000);
-var chat = document.getElementById("chat");
+var log = document.getElementById("log");
 document.getElementById("user").placeholder = name;
 
 var text = document.getElementById("text");
@@ -11,24 +11,49 @@ var now = function () {
 
 ws.onmessage = function (msg) {
     if(msg.data.startsWith("R:")){
-        var nums = msg.data.slice(2);
-        number.innerText = nums;
+        var nums = msg.data.slice(3).split(" ");
+        console.log(nums);
+        document.getElementById("dicel0").innerText = nums[0];
+        document.getElementById("dicel1").innerText = nums[1];
+        document.getElementById("dicel2").innerText = nums[2];
+        document.getElementById("dicel3").innerText = nums[3];
+        document.getElementById("dicel4").innerText = nums[4];
     }
     else{
         var line =  now() + " : " + msg.data + "\n";
-        chat.innerText += line;
+        log.innerText += line;
     }
 };
 
-text.onkeydown = function (e) {
-    if (e.keyCode === 13 && text.value !== "") {
-        var nowname = document.getElementById("user").value;
-        console.log(nowname);
-        ws.send("[" + ((nowname != "") ? nowname : name) + "] > " + text.value);
-        text.value = "";
-    }
-};
+// text.onkeydown = function (e) {
+//     if (e.keyCode === 13 && text.value !== "") {
+//         var nowname = document.getElementById("user").value;
+//         console.log(nowname);
+//         ws.send("[" + ((nowname != "") ? nowname : name) + "] > " + text.value);
+//         text.value = "";
+//     }
+// };
 
 function roll(){
-    ws.send("Roll");
+    console.log("rolling");
+    var dlis = "";
+    if(document.getElementById("dice0").checked){
+        dlis += "0"
+    } 
+    if(document.getElementById("dice1").checked){
+        dlis += "1"
+    } 
+    if(document.getElementById("dice2").checked){
+        dlis += "2"
+    }
+    if(document.getElementById("dice3").checked){
+        dlis += "3"
+    }
+    if(document.getElementById("dice4").checked){
+        dlis += "4"
+    }
+    if(dlis.length == 0) ws.send("Roll");
+    else {
+        ws.send("Reroll:" + dlis)
+    }
 }
--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
     "time"
     "math/rand"
     "strconv"
+    "strings"
 
     "github.com/gin-gonic/gin"
     "gopkg.in/olahol/melody.v1"
 )
+
+var dices = [5]int {1,1,1,1,1}
+var rollcnt = 0
 
 func main() {
     log.Println("Websocket App Start!")
@@ -27,20 +31,20 @@ func main() {
         m.HandleRequest(ctx.Writer, ctx.Request)
     })
 
-    m.HandleMessage(func(s *melody.Session, msg []byte) {
+    m.HandleMessage(func (s *melody.Session, msg []byte) {
         msgs := string(msg)
         log.Printf(msgs)
         if msgs == "Roll" {
-            rand.Seed(time.Now().UnixNano())
-            msgs = "R:"
-            for i := 0; i < 5; i++ {
-                msgs += " "  + strconv.Itoa(rand.Intn(6) + 1)
-            }
-            m.Broadcast([]byte(msgs))
+            rollDice("01234")
+            m.Broadcast(getDiceText())
+        } else if strings.HasPrefix(msgs, "Reroll:") {
+            rollDice(msgs[7:])
+            m.Broadcast(getDiceText())
         } else {
             m.Broadcast(msg)
         }
     })
+
 
     m.HandleConnect(func(s *melody.Session) {
         log.Printf("websocket connection open, [session: %#v]\n", s)
@@ -54,3 +58,26 @@ func main() {
 
     fmt.Println("WebSocket App End")
 }
+
+func rollDice(dicestr string) {
+    if rollcnt == 0 {
+        dicestr = "01234"
+    }
+    // rollcnt = (rollcnt + 1) % 3 // 実際の挙動
+    rollcnt++ // デバック用
+    rand.Seed(time.Now().UnixNano())
+    for _, c := range dicestr {
+        n := rand.Intn(6) + 1
+        i := int(c - '0') // c : rune
+        dices[i] = n
+    }
+}
+
+func getDiceText() []byte {
+    text := "R:"
+    for _, i := range dices {
+        text += " " + strconv.Itoa(i)
+    }
+    return []byte(text)
+}
+
